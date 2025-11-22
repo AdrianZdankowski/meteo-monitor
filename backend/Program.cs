d587f8af857cae87c871afbdaabc3d1214a8cb9e
unknown <stefoo123@outlook.com>
2025-11-22 10:46:34 +0100

    Added nginx config file, dockerized frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options => {
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173") // Allow Vite frontend
+            policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials(); // Required for SignalR
