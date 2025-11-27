361d27dcf7d34c59ea1e66795988962a4f9d10b9
unknown <stefoo123@outlook.com>
2025-11-27 19:43:28 +0100

    Env variables cleanup

--- regenerate-nethereum.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Regenerate Nethereum C# classes from smart contract ABI
-REM This batch file calls the PowerShell script
-
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0regenerate-nethereum.ps1"
-pause
